38b55a8c85e1862f21abdd3a2cd080a83c20be8b
DAB0mB <emanor6@gmail.com>
2018-10-28 17:33:02 +0800

    Step 1.2: Define parse functions stubs

--- src/index.js
+++ src/index.js
@@ -3,3 +3,39 @@ const types = {
   value: 'value',
   props: 'props',
 }
+
+const parseElement = (str) => {
+  let match
+  let length
+
+  const node = {
+    type: types.element,
+    props: parseProps(''),
+    children: [],
+    length: 0,
+    name: '',
+  }
+
+  return node
+}
+
+const parseProps = (str) => {
+  let match
+  let length
+
+  const node = {
+    type: types.props,
+    length: 0,
+    props: {},
+  }
+
+  return node
+}
+
+const parseValue = (str) => {
+  return {
+    type: types.value,
+    length: str.length,
+    value: str.trim(),
+  }
+}
